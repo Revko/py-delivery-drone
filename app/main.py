dde69e1083342682aa8769c3c99110e25833582f
Ivan Revko <revko7692@gmail.com>
2025-02-06 15:28:01 +0200

    change after cheak mentor

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ class BaseRobot:
             self,
             name: str,
             weight: int,
-            coords: list = None
+            coords: list[int] = None
     ) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -38,7 +38,7 @@ class FlyingRobot(BaseRobot):
             self,
             name: str,
             weight: int,
-            coords: list = None
+            coords: list[int] = None
     ) -> None:
         if coords is None:
             coords = [0, 0, 0]
@@ -58,7 +58,7 @@ class DeliveryDrone(FlyingRobot):
             weight: int,
             max_load_weight: int,
             current_load: Cargo = None,
-            coords: list = None
+            coords: list[int] = None
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
